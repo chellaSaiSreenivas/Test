211a34626ba168bfaa5cd663602a3cd515ef7981
chellaSaiSreenivas <83277551+chellaSaiSreenivas@users.noreply.github.com>
2025-01-21 15:46:49 +0530

    Updated Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
-import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  base: 'Test', // Replace 'repo-name' with your repository name
-  plugins: [ghPages],
+  base: './',
 });
